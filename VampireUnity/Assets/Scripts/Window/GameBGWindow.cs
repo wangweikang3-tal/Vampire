7bc8f5f48079c56d53e59e5b600278e56c765449
wangweikang3 <wangweikang3@tal.com>
2025-04-26 15:48:16 +0800

    role界面1

--- VampireUnity/Assets/Scripts/Window/GameBGWindow.cs
+++ VampireUnity/Assets/Scripts/Window/GameBGWindow.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameBGWindow : BasePanel
 {
     public GameBGWindow() : base(new UIType("Prefabs/Gamebg")) { }
+    
+    public override void OnEnter()
+    {
+        Debug.Log("GameBGWindow OnEnter");
+        UITool.S.GetChildGameObject("GameBGContinueBtn").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            PanelManager.S.PushPanel(new RoleWindow());
+        });
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("GameBGWindow OnExit");
+        // Add your cleanup code here
+    }
+
+    public override void OnPause()
+    {
+       UITool.S.GetChildGameObject("content").SetActive(false);
+       UITool.S.GetChildGameObject("title").SetActive(false);
+       UITool.S.GetChildGameObject("GameBGContinueBtn").SetActive(false);
+    }
 }
---
+++ VampireUnity/Assets/Scripts/Window/RoleWindow.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class RoleWindow : BasePanel
+{
+    public RoleWindow(): base(new UIType("Prefabs/RoleWindow")) { }
+}
