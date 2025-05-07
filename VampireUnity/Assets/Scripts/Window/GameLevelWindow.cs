acf47cc22a088cba529492ce703f630af782e13a
wangweikang3 <wangweikang3@tal.com>
2025-05-07 23:48:10 +0800

    串联场景

--- VampireUnity/Assets/Scripts/Window/GameLevelWindow.cs
+++ VampireUnity/Assets/Scripts/Window/GameLevelWindow.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameLevelWindow : BasePanel
 {
     public GameLevelWindow(): base(new UIType("Prefabs/Window/GameLevel")) { }
+    public override void OnEnter()
+    {
+        UITool.S.GetChildGameObject("GameLevelFight").gameObject.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            GameObject.Find("UIRoot").SetActive(false);
+            //跳转到Fight场景
+            SceneManager.LoadScene("FightScene");
+        });
+    }
 }
