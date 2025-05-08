7050cf242d4e4cca8ccd772795340666fa2fdcc2
wangweikang3 <wangweikang3@tal.com>
2025-05-08 10:43:28 +0800

    修复EquipBase装备属性为空的bug，并优化初始化逻辑

--- VampireUnity/Assets/Scripts/Equip/BagGrid.cs
+++ VampireUnity/Assets/Scripts/Equip/BagGrid.cs
@@ -5,7 +5,12 @@ using UnityEngine.UI;
 public class BagGrid : MonoBehaviour
 {
     [NonSerialized]public int EquipId;
-    
+
+    private void Awake()
+    {
+        
+    }
+
     public void OnClick()
     {
         //transform.Find("BagGridImage").GetComponent<Image>().
